32be8f4dcb8d2a3afc536079db702801d17834ac
ptrhaney <84342049+ptrhaney@users.noreply.github.com>
2021-08-30 16:00:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -133,8 +133,8 @@ require([
           field: "mag",
           axis: "all",
           stops: [
-            { value: 5.5, size: 40000, label: "<15%" },
-            { value: 7, size: 150000, label: "25%" }
+            { value: 5.5, size: 20000, label: "<15%" },
+            { value: 7, size: 50000, label: "25%" }
           ]
         },
         {
@@ -144,7 +144,7 @@ require([
             title: "Magnitude"
           },
           stops: [
-            { value: 7, color: [254, 240, 217], label: "4.5 - 7" },
+            { value: 6, color: [254, 240, 217], label: "4.5 - 7" },
             { value: 8, color: [179, 0, 0], label: ">8" }
           ]
         }
